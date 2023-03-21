dcd8aa48f55d1f13d50782d75ce3020c26c62c8b
Debtanaya <117731131+Debtanaya@users.noreply.github.com>
2023-03-21 13:53:56 +0530

    Add files via upload

---
+++ Backend/src/test/java/com/niit/Backend/UserTest.java
@@ -0,0 +1,61 @@
+package com.niit.Backend;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.Backend.DAO.UserDAO;
+import com.niit.Backend.modal.User;
+
+public class UserTest 
+{
+	private static AnnotationConfigApplicationContext context;
+	static User user;
+	static UserDAO userDAO;	
+	
+	@BeforeClass
+	public static void init() 
+	{
+		context = new AnnotationConfigApplicationContext();
+		context.scan("com.niit.Backend");
+		context.refresh();
+		
+		userDAO = (UserDAO)context.getBean("userDAO");
+	}
+	
+	@Test
+	public void testInsert()
+	{
+		user = new User();
+		/*
+		user.setContactNumber("1234567890");
+		user.setEmail("rohan@gmail.com");
+		user.setEnabled(true);
+		user.setFirstName("Rohan");
+		user.setId(1);
+		user.setLastName("Mishra");
+		user.setPassword("user12345");
+		user.setRole("USER");
+		*/
+		
+		user.setContactNumber("9807898734");
+		user.setEmail("rs@gmail.com");
+		user.setEnabled(true);
+		user.setFirstName("Rajiv");
+		user.setLastName("Singh");
+		user.setPassword("user12345");
+		user.setRole("USER");
+		
+		
+		assertEquals("Error inserting user" ,  true , userDAO.insert(user));
+	}
+	
+	//@Test
+	public void testGetUser()
+	{
+		user = userDAO.getUser("rohan@gmail.com");
+		assertEquals("Error fetching user" , "Rohan" , user.getFirstName());
+	}
+}	
