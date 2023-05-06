b8c1208c0df5670cc92ad09c1d3540ec336c431b
Debtanaya <117731131+Debtanaya@users.noreply.github.com>
2023-05-06 19:34:52 +0530

    Add files via upload

--- Backend/src/test/java/com/niit/Backend/UserTest.java
+++ Backend/src/test/java/com/niit/Backend/UserTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.niit.Backend.DAO.UserDAO;
+import com.niit.Backend.modal.Address;
 import com.niit.Backend.modal.User;
 
 public class UserTest 
@@ -34,25 +35,31 @@ public class UserTest
 		user.setEmail("rohan@gmail.com");
 		user.setEnabled(true);
 		user.setFirstName("Rohan");
-		user.setId(5);
 		user.setLastName("Mishra");
 		user.setPassword("user12345");
-		user.setRole("user");
+		user.setRole("USER");
 		*/
-		
-		
-		user.setContactNumber("1234567890");
-		user.setEmail("ag@gmail.com");
+		/*
+		user.setContactNumber("9807898734");
+		user.setEmail("sd@gmail.com");
 		user.setEnabled(true);
-		user.setFirstName("Ajita");
-		user.setLastName("Ghosh");
-		user.setPassword("ag12345");
-		user.setRole("supplier");
-		
-		
-		
+		user.setFirstName("Soham");
+		user.setLastName("Das");
+		user.setPassword("supplier12345");
+		user.setRole("SUPPLIER");
+		*/
+		/*
+		user.setContactNumber("9087667890");
+		user.setEmail("niitjadavpur@gmail.com");
+		user.setEnabled(true);
+		user.setFirstName("Niit");
+		user.setLastName("Jadavpur");
+		user.setPassword("admin12345");
+		user.setRole("ADMIN");
 		assertEquals("Error inserting user" ,  true , userDAO.insert(user));
+	*/
 	}
+	/*
 	
 	//@Test
 	public void testGetUser()
@@ -60,5 +67,26 @@ public class UserTest
 		user = userDAO.getUser("rohan@gmail.com");
 		assertEquals("Error fetching user" , "Rohan" , user.getFirstName());
 	}
-	
+	*/
+	@Test
+	public void testAddUser() 
+	{
+		
+		Address address = new Address();
+		address.setAddressLineOne("101/B Jadoo Society, Krissh Nagar");
+		address.setAddressLineTwo("Near Kaabil Store");
+		address.setCity("Mumbai");
+		address.setState("Maharashtra");
+		address.setCountry("India");
+		address.setPostalCode("400001");
+		address.setShipping(true);
+		
+		address.setUserId(33);
+			
+		assertEquals("Failed to add the billing address!", true, userDAO.insertAddress(address));
+		
+		
+		
+		
+	}
 }	
